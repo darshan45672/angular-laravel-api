de9f8a5a85540620944e1500872adea58ac46acf
darshan45672 <drshnbhandary@gmail.com>
2025-07-09 23:23:45 +0530

    implement api integration

--- frontend/src/app/post/post-service.ts
+++ frontend/src/app/post/post-service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from './post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  private apiUrl = 'http://127.0.0.1:8000/api'
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Post []> {
+    return this.http.get<Post[]>(`${this.apiUrl}/posts`);
+  }
 }
