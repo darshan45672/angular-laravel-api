6f2d259c8697576f268adc907bc2a1279c0d44a7
darshan45672 <drshnbhandary@gmail.com>
2025-07-09 19:46:21 +0530

    added componenst and boostrap angualr

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Index } from './post/index';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "posts", component: Index }
+];
